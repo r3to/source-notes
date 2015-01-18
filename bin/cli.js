b29656ac8861f39f136ef851eb7da483cc15db64
Akiva Levy <akiva@sixthirteen.co>
2015-01-18 13:57:39 -0800

    Initial commit

---
+++ bin/cli.js
@@ -0,0 +1,83 @@
+#!/usr/bin/env node
+
+'use strict';
+
+var fs = require('fs');
+var pkg = require('../package.json');
+var chalk = require('chalk');
+var tree = require('pretty-tree');
+var argv = require('minimist')(process.argv.slice(2), {
+  alias: { h: 'help', v: 'version', a: 'annotation' }
+});
+
+if (argv.version) {
+  console.log(pkg.version);
+  return false;
+}
+
+if (argv.h) {
+  var usage = fs.readFileSync(__dirname + '/usage.txt', 'utf8');
+  console.log(usage);
+  return false;
+}
+
+// If no path is specified, use cwd
+if (!argv._[0]) argv._[0] = '.';
+
+searchDirectory(argv._[0]);
+
+function isValidSourceFile(file) {
+  return /.*\.(js|coffee|iced|styl|jade|ejs)$/.test(file);
+}
+
+function searchDirectory(dir) {
+  if (/.*\/node_modules\/.*$/.test(dir)) return false;
+  fs.readdir(dir, function (err, files) {
+    if (err) error(err);
+
+    files.forEach(function (file) {
+      var path = dir + '/' + file;
+      fs.stat(path, function (err, stat) {
+        if (err) error(err);
+        if (stat && stat.isDirectory()) searchDirectory(path);
+        if (stat && stat.isFile() && isValidSourceFile(path)) searchFile(path);
+      });
+    });
+  });
+}
+
+function formatSourceLine(line, type, code) {
+  type = type.toUpperCase();
+  var labels = {
+    TODO: chalk.green('TODO'),
+    FIXME: chalk.red('FIXME'),
+    OPTIMISE: chalk.yellow('OPTIMISE'),
+    OPTIMIZE: chalk.yellow('OPTIMIZE')
+  };
+  var str = line + ': ';
+  str += !!labels[type] ? labels[type] : chalk.white.bold(type);
+  str += ' ' + code;
+  return str;
+}
+
+function searchFile(file) {
+  var types = argv.a ?
+    argv.a.replace(/,/g, '|') :
+    'todo|fixme|optimise|optimize';
+  var regex = new RegExp('((\\/\\/|\\*)\\s?(' + types + '))\\W*(.*$)', 'i');
+  var items = [];
+  var lines = fs.readFileSync(file).toString().split('\n');
+
+  lines.forEach(function (line, i) {
+    var match = line.match(regex);
+    if (match) items.push(formatSourceLine(i + 1, match[3], match[4]));
+  });
+
+  if (items.length) console.log(tree({ label: file, leaf: items }));
+}
+
+function error(err) {
+  if (!err) return;
+  console.error(String(err));
+  process.exit(1);
+}
